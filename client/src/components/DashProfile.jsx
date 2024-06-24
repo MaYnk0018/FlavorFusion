8ad143d36e7a39552d2bdae2c42a600a9c98b082
MaYnk0018 <126932719+MaYnk0018@users.noreply.github.com>
2024-06-24 16:18:28 +0530

    SignOut at all Position

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -9,6 +9,7 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import {
+  signoutSuccess,
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
@@ -86,6 +87,21 @@ export default function DashProfile() {
       dispatch(deleteUserFailure(error.message));
     }
   }
+  async function handleSignout(){
+    try{
+      const res= await fetch('/api/user/signout',{
+        method:'POST'
+      });
+      const data= await res.json();
+      if(!res.ok){
+        console.log(data.message);
+      }else{
+        dispatch(signoutSuccess(data))
+      }
+    }catch(error){
+      console.log(error);
+    }
+  }
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -159,7 +175,7 @@ export default function DashProfile() {
         <span className="cursor-pointer" onClick={() => setShowModal(true)}>
           Delete Account
         </span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span className="cursor-pointer" onClick={handleSignout}>Sign Out</span>
       </div>
       <Modal show={showModal} onClose={()=>setShowModal(false)} popup size="md">
         <Modal.Header />
